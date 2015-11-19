6be7caaecf105ae25de4fd90f3ffece349eab33e
FirstBitStudios <firstbitstudios@gmail.com>
2015-11-18 23:43:42 -0500

    TestGame code 2

---
+++ TestGame/app/src/main/java/android/fbstudios/testgame/MainActivity.java
@@ -0,0 +1,41 @@
+package android.fbstudios.testgame;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //turn title off
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //set to full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(new GamePanel(this));
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
