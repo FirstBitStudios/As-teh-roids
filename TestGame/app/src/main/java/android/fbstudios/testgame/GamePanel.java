6cba2d093adb99b23604c8c817649296ea01bfd8
FirstBitStudios <firstbitstudios@gmail.com>
2015-11-18 23:21:35 -0500

    TestGame code Part 2

--- TestGame/app/src/main/java/android/fbstudios/testgame/GamePanel.java
+++ TestGame/app/src/main/java/android/fbstudios/testgame/GamePanel.java
@@ -5,28 +5,58 @@ import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-/**
- * Created by kenny on 11/18/2015.
- */
-public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
+
+public class GamePanel extends SurfaceView implements SurfaceHolder.Callback
+{
+    private MainThread thread;
     public GamePanel(Context context)
     {
         super(context);
+
+
+        //add the callback to the surfaceholder to intercept events
         getHolder().addCallback(this);
+
+        thread = new MainThread(getHolder(), this);
+
+        //make gamePanel focusable so it can handle events
+        setFocusable(true);
     }
 
     @Override
-        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height){}
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height){}
 
     @Override
-        public void surfaceDestroyed(SurfaceHolder holder){}
-
-    @Override
-        public void surfaceCreated(SurfaceHolder holder){}
-
-    @Override
-        public boolean onTouchEvent(MotionEvent event)
+    public void surfaceDestroyed(SurfaceHolder holder){
+        boolean retry = true;
+        while(retry)
         {
-            return super.onTouchEvent(event);
+            try{thread.setRunning(false);
+                thread.join();
+
+            }catch(InterruptedException e){e.printStackTrace();}
+            retry = false;
         }
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder){
+
+        //we can safely start the game loop
+        thread.setRunning(true);
+        thread.start();
+
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        return super.onTouchEvent(event);
+    }
+    public void update()
+    {
+
+
+
+    }
 }
